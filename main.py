d99ad4bdd6170e071bc5d25358e40a6750caba78
Asakikior <x50934712@gmail.com>
2023-10-21 17:22:13 +0300

    Create file

---
+++ main.py
@@ -0,0 +1,57 @@
+import pygame
+
+
+
+WIDTH = 1300
+HEIGHT = 700
+
+
+
+SIZE = (WIDTH, HEIGHT)
+
+window = pygame.display.set_mode(SIZE)
+background_color = (0,255,0)
+window.fill(background_color)
+clock = pygame.time.Clock()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
